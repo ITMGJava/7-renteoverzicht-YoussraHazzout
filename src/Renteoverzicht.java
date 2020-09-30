99fb4046729271faf15ebb75423a9e2226f54b0d
YoussraHazzout <YoussraHazzout@gmail.com>
2020-09-30 16:19:28 +0200

    Finale versie. Na feedback tijdens de les. Programma geeft nu een range tussen 100 en 1000. Wanneer iemand iets fout invoert, komt er een foutmelding en kan men het nogmaals proberen.

--- src/Renteoverzicht.java
+++ src/Renteoverzicht.java
@@ -10,25 +10,32 @@ public class Renteoverzicht {
         System.out.println(naam + ", leuk u te ontmoeten! :-)");
         for (; ; ) {
             System.out.println(naam + ", voer een start bedrag in om de rente per jaar te berekenen.");
+            System.out.println("Let op! Het bedrag moet tussen de €100 en €1000 liggen.");
             float startBedrag = s.nextInt();
-
+if (startBedrag < 100){
+    System.out.println("Helaas, "+naam+", het minimale bedrag om de spaarrente te berekenen is €100, probeer het nogmaals.");
+}else{if(startBedrag > 1000){
+    System.out.println("Helaas,"+naam+", het maximale bedrag om de spaarrente te berekenen is €1000, probeer het nogmaals.");
+}}
             for (double i = 1; i <= 10; i++) {
-                if (startBedrag <= 299) {
+
+                if (startBedrag >= 100 && startBedrag <= 299 ) {
                     System.out.println("Met een rente van 3% per jaar is het bedrag:");
                     var spaarRente = (float) (startBedrag * i * 3) / 100 + startBedrag;
                     System.out.println("Jaar " + i + ": €" + spaarRente);
                 } else {
-                    if (startBedrag <= 599) {
+                    if (startBedrag >= 300 && startBedrag <=599) {
                         System.out.println("Met een rente van 2% per jaar is het bedrag:");
                         var spaarRente = (float) (startBedrag * i * 2) / 100 + startBedrag;
                         System.out.println("Jaar " + i + ": €" + spaarRente);
 
+                    }else {
+                        if (startBedrag >= 599 && startBedrag <=1000) {
+                            System.out.println("Met een rente van 1% per jaar is het bedrag:");
+                            var spaarRente = (float) (startBedrag * i * 1) / 100 + startBedrag;
+                            System.out.println("Jaar " + i + ":  €" + spaarRente);
+                        }
                     }
-                }
-                if (startBedrag > 599) {
-                    System.out.println("Met een rente van 1% per jaar is het bedrag:");
-                    var spaarRente = (float) (startBedrag * i * 1) / 100 + startBedrag;
-                    System.out.println("Jaar " + i + ":  €" + spaarRente);
                 }
 
             }
